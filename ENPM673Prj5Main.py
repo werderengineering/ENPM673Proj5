b0dba396b3aa775cfeef61c075d68eec5028a240
Werder <werdertimothy@gmail.com>
2020-04-22 12:31:58 -0600

    Init

---
+++ ENPM673Prj5Main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+from __main__ import *
+import matplotlib.pyplot as plt
+import imutils
+import os
+import random
+
+
+
+
+
+print('Imports Complete')
+
+print('CV2 version')
+print(cv2.__version__)
+
+flag = False
+prgRun = True
+flag_trackingFeature = True
+
+def main(prgRun):
+    # start file
+    print("Run")
+
+    prgRun = False
+    return prgRun
+
+
+
+
+print('Function Initializations complete')
+
+if __name__ == '__main__':
+    print('Start')
+    prgRun = True
+    while prgRun == True:
+        prgRun = main(prgRun)
+
+    print('Goodbye!')
+    cv2.destroyAllWindows()
