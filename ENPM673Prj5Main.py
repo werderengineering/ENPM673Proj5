a278c40b466c3e9ae75db0ffe657c322d46823ca
Werder <werdertimothy@gmail.com>
2020-04-24 15:06:54 -0600

    Final init with images corrected

--- ENPM673Prj5Main.py
+++ ENPM673Prj5Main.py
@@ -1,10 +1,16 @@
 import numpy as np
 import cv2
-from __main__ import *
 import matplotlib.pyplot as plt
 import imutils
 import os
+import sys
 import random
+import pickle
+
+
+from Oxford_dataset.ReadCameraModel import *
+from Oxford_dataset.UndistortImage import *
+from imageCorrection import *
 
 print('Imports Complete')
 
@@ -19,6 +25,52 @@ prgRun = True
 def main(prgRun):
     # start file
     print("Run")
+    # Hard input stand-ins
+    directory = "./Oxford_dataset/stereo/centre/"
+    FixImagery=True
+    ###########################
+    # Soft inputs
+    # This is a pain
+
+    # getdataYN = str.lower(input('Create a data set? Enter |yes| or |no|: '))
+    # if getdataYN == 'yes':
+    #     FixImagery = True
+    #
+    # directory = str(input(
+    #     'What is the name and directory of the folder with the images? Note, this should be entered as"./folder_name if on Windows". Example: ./Bolt2/img: \n'))
+
+    ###########################
+    fx, fy, cx, cy, G_camera_image, LUT = ReadCameraModel('./Oxford_dataset/model')
+
+    if FixImagery:
+        correctImages(directory, LUT)
+
+
+    frameset = openVar('CorrectedFrames')
+
+    for i, frame in enumerate(frameset):
+        ###########################
+        # Add Functions here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ###########################
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+
 
     prgRun = False
     return prgRun
